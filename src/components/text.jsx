4ab38f132086f7b9f7d6204d8b4b27834ab1b33d
John Angel Henao <jhon.henao@omni.pro>
2024-03-12 20:23:24 -0500

    prueba

--- src/components/text.jsx
+++ src/components/text.jsx
@@ -3,11 +3,12 @@ import React from "react";
 // Asegúrate de aceptar props en tu componente
 const Text = (props) => {
     // Convertimos el objeto props a una cadena de texto para mostrarlo
-    const propsAsString = JSON.stringify(props, null, 2); // El segundo argumento `null` y el tercero `2` son para formatear el JSON
+    const propsAsString = JSON.stringify(props, null, 2);
+    console.log(propsAsString) // El segundo argumento `null` y el tercero `2` son para formatear el JSON
 
     return (
         <div>
-            <p>Bienvenido al microfrontend tres</p>
+            <p>Bienvenido al microfrontend tres prueba props AEM</p>
             {/* Mostramos las props formateadas dentro de un elemento <pre> para mejor legibilidad */}
             <pre>{propsAsString}</pre>
         </div>
