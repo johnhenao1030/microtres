d7cdfc051b0d2bab3985d2d1bd49860f7c0dfa26
John Angel Henao <jhon.henao@omni.pro>
2024-05-04 13:11:08 -0500

    se agrega estilos al proyecto

--- src/components/text.jsx
+++ src/components/text.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import '../styles/form.css';
 
 const Text = ({ appData }) => {
     const [formData, setFormData] = useState({
